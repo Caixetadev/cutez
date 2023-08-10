a0d58dfd9e10d370c9269a9be96cd969aab59778
Rafael Caixeta <caixetadev@gmail.com>
2023-08-10 20:56:50 +0200

    feat: add next auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,29 @@
+import type { NextAuthOptions } from 'next-auth'
+
+import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import AppleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+    error: '/login',
+  },
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    AppleProvider({
+      clientId: process.env.APPLE_ID!,
+      clientSecret: process.env.APPLE_SECRET!,
+    }),
+  ],
+}
