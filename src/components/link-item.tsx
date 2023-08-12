76e36fc79c1f98986ca1349438bcca3b3451cd87
Rafael Caixeta <caixetadev@gmail.com>
2023-08-12 20:59:26 +0200

    feat: add link item

---
+++ src/components/link-item.tsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardContent,
+  CardTitle,
+} from '@/components/ui/card'
+import { Copy } from '@/components/copy'
+import { LinkOperations } from '@/components/link-operations'
+import { Link } from '@prisma/client'
+
+interface LinkItemProps {
+  data: Link[]
+}
+
+export function LinkItem({ data }: LinkItemProps) {
+  return (
+    <div className='flex flex-col gap-4'>
+      {data.map((item) => (
+        <Card key={item.id} className='w-full'>
+          <CardHeader className='flex flex-col justify-center'>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center'>
+                <CardTitle>{item.domain}</CardTitle>
+                <Copy text={item.url} />
+              </div>
+              <div>
+                <LinkOperations
+                  id={item.id}
+                  description={item.description as string}
+                  domain={item.domain}
+                  url={item.url}
+                />
+              </div>
+            </div>
+            <CardDescription>{item.url}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className='text-muted-foreground'>{item.description}</p>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  )
+}
