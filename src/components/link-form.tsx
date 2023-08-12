18a9cc67c8225ca800385518b1a2b6f7723da3b9
Rafael Caixeta <caixetadev@gmail.com>
2023-08-12 21:00:31 +0200

    feat: add form to create link

---
+++ src/components/link-form.tsx
@@ -0,0 +1,121 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+
+import { LinkSchema, LinkForm } from '@/lib/validations/link'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { useRouter } from 'next/navigation'
+import { toast } from './ui/use-toast'
+import { Dispatch, SetStateAction } from 'react'
+
+interface LinkFormProps {
+  defaultValues?: LinkForm
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export function LinkForm(props: LinkFormProps) {
+  const { defaultValues, setIsOpen } = props
+
+  const form = useForm<LinkForm>({
+    resolver: zodResolver(LinkSchema),
+    defaultValues: defaultValues ?? { description: '', domain: '', url: '' },
+  })
+
+  const router = useRouter()
+
+  async function onSubmit(data: LinkForm) {
+    const response = await fetch('/api/link', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        data,
+      }),
+    })
+
+    if (response.ok) {
+      router.refresh()
+      toast({
+        description: 'Link create with success',
+      })
+      setIsOpen(false)
+      return
+    }
+
+    form.setError('domain', {
+      message: 'Existe um domain com esse nome ja',
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className='flex flex-col gap-4 py-4'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name='url'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor='url' className='text-right'>
+                URL
+              </FormLabel>
+              <FormControl>
+                <Input id='url' placeholder='https://' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='domain'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor='domain' className='text-right'>
+                Domain
+              </FormLabel>
+              <FormControl>
+                <Input id='domain' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='description'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor='description' className='text-right'>
+                Description
+              </FormLabel>
+              <FormControl>
+                <Textarea id='description' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit'>{defaultValues ? 'Save' : 'Create'}</Button>
+      </form>
+    </Form>
+  )
+}
