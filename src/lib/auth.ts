5dcb3c30db690aa6f0d829c34a1668bee4a00a45
Rafael Caixeta <caixetadev@gmail.com>
2023-08-12 20:54:25 +0200

    feat: add id in session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,4 +27,37 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.APPLE_SECRET!,
     }),
   ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+  },
 }
