1c6e20c7b28ed50a7f87cb71062e3c0fc8179085
Rafael Caixeta <caixetadev@gmail.com>
2023-08-13 20:21:40 +0200

    refactor: changing div to header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,13 +5,13 @@ import { Button } from '@/components/ui/button'
 
 export function Navbar() {
   return (
-    <div className='container'>
-      <div className='flex items-center justify-between py-6'>
+    <header className='container'>
+      <div className='flex h-16 flex-wrap items-center justify-between py-4'>
         <Logo />
         <Button asChild variant='secondary'>
           <Link href='/login'>Login</Link>
         </Button>
       </div>
-    </div>
+    </header>
   )
 }
