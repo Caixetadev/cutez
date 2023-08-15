2cc34429681a01457389f07e117fd8844209452a
Rafael Caixeta <caixetadev@gmail.com>
2023-08-15 06:05:37 +0200

    refactor: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,43 +6,38 @@ export default withAuth(
   async function middleware(req: NextRequest, ev: NextFetchEvent) {
     const token = await getToken({ req })
     const isAuth = !!token
-    const isAuthPage = req.nextUrl.pathname.startsWith('/login')
-    // req.nextUrl.pathname.startsWith('/register')
-    const slug = req.nextUrl.pathname.split('/')
 
-    if (slug[1] === 'go') {
+    if (req.nextUrl.pathname.startsWith('/dashboard') && !isAuth) {
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    if (req.nextUrl.pathname.startsWith('/login') && isAuth) {
+      return NextResponse.redirect(new URL('/dashboard', req.url))
+    }
+
+    const pages = ['/login', '/dashboard', '/']
+
+    if (
+      !pages.includes(req.nextUrl.pathname) &&
+      !req.nextUrl.pathname.startsWith('/api')
+    ) {
       try {
         const response = await fetch(
-          `${req.nextUrl.origin}/api/link/${slug[slug.length - 1]}`
+          `${req.nextUrl.origin}/api/link/${req.nextUrl.pathname.replace(
+            '/',
+            ''
+          )}`
         )
         const data = await response.json()
 
-        if (!response.ok) {
-          throw new Error(response.statusText)
-        }
-
-        if (data.status === 404) {
+        if (!response.ok || data.status === 404 || !data?.url) {
           return NextResponse.redirect(req.nextUrl.origin)
         }
 
-        if (data?.url) {
-          return NextResponse.redirect(new URL(data?.url))
-        }
-      } catch (error: any) {
-        return new Response(error.message, { status: 429 })
+        return NextResponse.redirect(new URL(data.url))
+      } catch (error) {
+        return new NextResponse('Rate limit')
       }
-    }
-
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL('/dashboard', req.url))
-      }
-
-      return null
-    }
-
-    if (!isAuth) {
-      return NextResponse.redirect(new URL(`/login`, req.url))
     }
   },
   {
@@ -58,5 +53,5 @@ export default withAuth(
 )
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/login', '/go/:slug*'],
+  matcher: ['/dashboard', '/login', '/:slug*'],
 }
