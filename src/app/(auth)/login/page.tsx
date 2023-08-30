f2f909d0f711300563df391815efa02d7d5dd9e8
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-08-30 20:31:56 +0530

    commented out the register link no register page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -43,14 +43,14 @@ export default function LoginPage() {
         <CardContent className='flex flex-col space-y-4'>
           <UserAuthForm />
 
-          <p className='px-8 text-center text-sm text-muted-foreground'>
+          {/* <p className='px-8 text-center text-sm text-muted-foreground'>
             <Link
               href='/register'
               className='hover:text-brand underline underline-offset-4'
             >
               Don&apos;t have an account? Sign Up
             </Link>
-          </p>
+          </p> */}
         </CardContent>
       </Card>
     </div>
