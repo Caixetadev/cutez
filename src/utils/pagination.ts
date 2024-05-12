2eee4012a298a6070c23002b58017b728d0d7fef
Rafael Caixeta <caixetadev@gmail.com>
2024-05-12 21:44:29 +0200

    chore: remove comments

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -15,24 +15,24 @@ export const getPageNumbers = ({
     return { pages: Array.from({ length: totalPages }, (_, i) => i + 1) }
   } else {
     const half = Math.floor(totalPagesToDisplay / 2)
-    // To ensure that the current page is always in the middle
+
     let start = currentPage - half
     let end = currentPage + half
-    // If the current page is near the start
+
     if (start < 1) {
       start = 1
       end = totalPagesToDisplay
     }
-    // If the current page is near the end
+
     if (end > totalPages) {
       start = totalPages - totalPagesToDisplay + 1
       end = totalPages
     }
-    // If showLeftEllipsis is true, add an ellipsis before the start page
+
     if (showLeftEllipsis) {
       start++
     }
-    // If showRightEllipsis is true, add an ellipsis after the end page
+
     if (showRightEllipsis) {
       end--
     }
